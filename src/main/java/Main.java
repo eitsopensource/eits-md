aa8b3107ff8814e2d2e8de50d67e041ac56e1346
rodrigo <rodrigo@eits.com.br>
2015-03-13 18:30:16 -0300

    removed the test code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,9 @@ import java.net.MalformedURLException;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.webapp.Configuration;
+import org.eclipse.jetty.webapp.MetaInfConfiguration;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 public class Main extends ResourceHandler {
 
@@ -13,7 +16,7 @@ public class Main extends ResourceHandler {
 	public Resource getResource(String path) throws MalformedURLException {
 		Resource resource = Resource.newClassPathResource(path);
 		
-		if (resource == null || !resource.exists()) {
+		if ( resource == null || !resource.exists() ) {
 			resource = Resource.newClassPathResource("META-INF/resources"+ path);
 		}
 		return resource;
@@ -25,14 +28,44 @@ public class Main extends ResourceHandler {
 	 * @throws Exception
 	 */
 	public static void main(String[] args) throws Exception {
-		Server server = new Server(8080);
+		final Server server = new Server(8080);
+		
+		//-------------------
+		final WebAppContext webAppContext = new WebAppContext();
+		webAppContext.setContextPath("/");
+		
+		Configuration[] configs = new Configuration[1];
+		configs[0] = new MetaInfConfiguration();
+		webAppContext.setConfigurations( configs );
+		webAppContext.setParentLoaderPriority(true);
+        //webAppContext.setBaseResource(new ResourceCollection(resources));
+		
+//		DefaultServlet defaultServlet = new DefaultServlet() {
+//			private static final long serialVersionUID = -1239684760674097791L;
+//
+//			@Override
+//			public void service(ServletRequest arg0, ServletResponse resp) throws ServletException, IOException {
+//				if (resp instanceof HttpServletResponse) {
+//					HttpServletResponse r = (HttpServletResponse) resp;
+//					r.setHeader("Cache-Control", "no-cache");
+//				}
+//				super.service(arg0, resp);
+//			}
+//		};
+//		final ServletHolder holder = new ServletHolder(defaultServlet);
+//		holder.setInitParameter("useFileMappedBuffer", "false");
+//		holder.setInitParameter("org.eclipse.jetty.servlet.Default.cacheControl", "no-store,no-cache,must-revalidate");
+//		servletHandler.addServlet(holder, "/");
+		
 
-		final Main handler = new Main();
-		handler.setWelcomeFiles(new String[]{"index.html"});
-		handler.setResourceBase("/");
-
-		server.setHandler(handler);
-
+		//-------------------
+		final Main resourceHandler = new Main();
+		resourceHandler.setWelcomeFiles(new String[]{"index.html"});
+		resourceHandler.setResourceBase(".");
+		server.setHandler(resourceHandler);
+		
+		//server.setHandler(webAppContext);
+		
 		server.start();
 		server.join();
 	}
--- src/main/resources/META-INF/resources/webjars/eits-md/1.0.0/containers/box/demo/script.js
+++ src/main/resources/META-INF/resources/webjars/eits-md/1.0.0/containers/box/demo/script.js
@@ -5,8 +5,5 @@ angular.module('eits.containers.box.sample', [
 .controller('AppCtrl', function($scope) {
 	
 	$scope.changePosition = function() {
-		//angular.element('#bt')[0].attributes.x.value = 100;
-		console.log( $('#bt') );
-		$('#bt').attr('x', 100);
 	}
 });
