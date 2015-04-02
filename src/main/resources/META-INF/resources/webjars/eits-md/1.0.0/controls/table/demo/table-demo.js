7cf40674f8aa75932537332bc3cc52726a0d5762
Henrique Lobato <lobato.zago@gmail.com>
2015-04-02 10:00:00 -0300

    Funcionalidade de thumbnail e ajuste da largura como atributo das colunas.

--- src/main/resources/META-INF/resources/webjars/eits-md/1.0.0/controls/table/demo/table-demo.js
+++ src/main/resources/META-INF/resources/webjars/eits-md/1.0.0/controls/table/demo/table-demo.js
@@ -8,42 +8,49 @@ angular.module('eits.controls.table.sample', [
         $scope.content = [
             {
                 id: 1,
+                thumbnail: 'http://www.eits.com.br/images/eits-topo.png',
                 name: 'Bruno Mars',
                 description: 'Human',
                 last_modified: 'Jun 5, 2014'
             },
             {
                 id: 2,
+                thumbnail: 'http://www.eits.com.br/images/eits-topo.png',
                 name: 'AT-AT',
                 description: 'Robot',
                 last_modified: 'Jun 5, 2014'
             },
             {
                 id: 3,
+                thumbnail: 'http://www.eits.com.br/images/eits-topo.png',
                 name: 'Mark Ronson',
                 description: 'Human',
                 last_modified: 'Jun 5, 2014'
             },
             {
                 id: 4,
+                thumbnail: 'http://www.eits.com.br/images/eits-topo.png',
                 name: 'Daft Punk',
                 description: 'Human-Robot',
                 last_modified: 'Jun 5, 2014'
             },
             {
                 id: 5,
+                thumbnail: 'http://www.eits.com.br/images/eits-topo.png',
                 name: 'Lady Gaga',
                 description: 'Undefined',
                 last_modified: 'Jun 5, 2014'
             },
             {
                 id: 6,
+                thumbnail: 'http://www.eits.com.br/images/eits-topo.png',
                 name: 'Mark Ronson',
                 description: 'Human',
                 last_modified: 'Jun 5, 2014'
             },
             {
                 id: 7,
+                thumbnail: 'http://www.eits.com.br/images/eits-topo.png',
                 name: 'Mark Ronson',
                 description: 'Human',
                 last_modified: 'Jun 5, 2014'
@@ -76,6 +83,5 @@ angular.module('eits.controls.table.sample', [
                 .error(function (data, status, headers, config) {
 
                 });
-
         }
     });
