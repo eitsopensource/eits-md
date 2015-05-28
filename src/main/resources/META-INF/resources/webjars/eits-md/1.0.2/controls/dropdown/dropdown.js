0d7c85ff827247cb3c08648ef5b167ff485270d5
Eduardo Alves <eduardoa.alves@eits.com.br>
2015-05-28 17:55:49 -0300

    Correções diversas nos componentes Dropdown e Papersheet

--- src/main/resources/META-INF/resources/webjars/eits-md/1.0.2/controls/dropdown/dropdown.js
+++ src/main/resources/META-INF/resources/webjars/eits-md/1.0.2/controls/dropdown/dropdown.js
@@ -35,8 +35,84 @@
 	        controller: Controller,
 	        scope   : {}
 	    };
-        
-	/**
+
+    /**
+     * monta o nome da classe css de acordo com a posição dele na tela
+     * @param element
+     * @param menuElement
+     * @returns {string}
+     */
+    function getAnimationClass(element, menuElement) {
+        var width = menuElement.parent().width();
+        var height = menuElement.parent().height();
+        var offsetBottom = $document.height() - ( height + angular.element(menuElement[0]).offset().top );
+        var offsetRight = $document.width() - ( width + angular.element(menuElement[0]).offset().left );
+        var x = width > offsetRight ? 'right' : 'left';
+        var y = height > offsetBottom ? 'bottom' : 'top';
+
+        return x + '-' + y;
+
+    }
+
+    /**
+     * define a posição do menu em relação ao botão responsavel pelo evento de click
+     * @param button
+     * @param menu
+     * @param animationOrientation
+     */
+    function definePosition(button, menu, animationOrientation) {
+
+        var menuWidth = menu.width();
+        var menuHeight = menu.height();
+
+        var buttonWidth = button.width();
+        var buttonHeight = button.height();
+
+        var rect = button[0].getBoundingClientRect();
+
+
+        var position = {};
+
+        /*menu.css({
+            'left'  : '0px',
+            'top'   : '0px'
+        })*/
+
+        if( menuHeight > rect.height ) {
+            position.x = rect.top;
+            position.y = rect.left
+        }
+
+        menu.css({
+            'left'  : position.y + 'px',
+            'top'   : position.x + 'px'
+        });
+
+        /*if( animationOrientation == 'left-top' ){
+            menu.css({
+                'left'  : '0px',
+                'top'   : '15px'
+            })
+        }
+        else if( animationOrientation == 'right-top' ){
+            menu.css({
+                'left'  : (menuHeight + buttonHeight) * -1,
+                'top'   : '15px'
+            })
+        }else if( animationOrientation == 'right-bottom' ){
+            menu.css({
+                'left'  : (menuHeight + buttonHeight) * -1,
+                'top'   : (menuWidth + buttonWidth) * -1
+            })
+        }else if( animationOrientation == 'left-bottom'){
+            menu.css({
+                'left'  : '0px',
+                'top'   : (menuWidth + buttonWidth) * -1
+            })
+        }*/
+    }
+
+    /**
 	 *
 	 */
     function CompileHandler( element, attributes, transclude ) {
@@ -44,17 +120,34 @@
 
             pre: function preLink( scope, element, attributes, controller ) {
 
+                /**
+                 * adiciona atributo identificador único para o dropdown
+                 */
+                element.find('.drop').attr('id', 'drop_down_' + scope.$id);
+                console.log(element);
+
             },
-            post: function postLink( scope, element, attributes,controller, transclude ) {
+            post: function postLink( scope, element, attributes, controller, transclude ) {
 
                 var ddMenu = element.find('.dd-menu');
+
+                /**
+                 * adiciona atributo identificador único para o dropdown
+                 */
+                ddMenu.attr('id', '_drop_down_' + scope.$id);
+
+                /**
+                 * adiciona o elemento menu antes no final do body
+                 */
+                ddMenu.appendTo('body');
 
                 /**
                  * define o icone no template
                  */
                 scope.iconMenu = attributes.iconMenu;
+
                 /**
-                 * define a orientaçaão do menu do dropdown
+                 * define a orientação do menu do dropdown
                  */
                 var animateOrientation = getAnimationClass(element.find('.dd'), ddMenu)
 
@@ -63,19 +156,10 @@
                  */
                 scope.menuOrientation = attributes.menuOrientation == undefined ? animateOrientation : attributes.menuOrientation;
 
-                /**
-                 * verifica se existe width definido pelo usuário
-                 */
-                scope.menuWidth = attributes.menuWidth == undefined ? setWidth('250px') : setWidth(attributes.menuWidth);
-                /**
-                 * verifica se height width definido pelo usuário
-                 */
-                scope.menuHeight = attributes.menuHeight == undefined ? setHeight('250px') : setHeight(attributes.menuHeight);
-
-                definePosition(element.find('.dd'), ddMenu,animateOrientation);
+                definePosition(element.find('.dd').find('button'), ddMenu, animateOrientation);
 
                 /**
-                 * verifica se o evento do click está sendo feio pela div do icone
+                 * verifica se o evento do click está sendo feito pela div do ícone
                  */
                 $document.bind('click', function(ev) {
 
@@ -87,101 +171,36 @@
                          * remove a classe ativa do menu caso existir, e adiciona novamente o menu a partir do icone que está sendo clicado
                          */
                         $('.dd-menu').removeClass('active');
-                        if( $(ev.target).closest('eits-dropdown').length > 0 ) {
+                        if( $(ev.target).closest('eits-dropdown').length > 0 && !$(ev.target).is('li')) {
                             $(ev.target).closest('eits-dropdown').find('.dd-menu').addClass('active');
+
+
+                            var id = $(ev.target).parent().attr('id');
+
+                            definePosition($('#' + id), ddMenu, animateOrientation);
+
+                            $('div#_' + id).addClass('active');
 
                             ev.stopPropagation();
                         }
                     }
-                })
+                });
 
                 ddMenu.on('click', function(e){
-                        $(e.target).closest('eits-dropdown').find('.dd-menu').removeClass('active');
-                        e.stopPropagation();
+                    $(e.target).closest('eits-dropdown').find('.dd-menu').removeClass('active');
+                        //e.stopPropagation();
                 });
             }
         }
-        /**
-         * Seta o valor do width quando definido pelo usuaário
-         * @param menuWidth
-         */
-        function setWidth(menuWidth){
-            element.find('.dd-menu').css({
-                'width' : menuWidth
-            });
-        }
 
-        /**
-         * Seta o valor do height quando definido pelo usuaário
-         * @param menuHeight
-         */
-        function setHeight(menuHeight){
-            element.find('.dd-menu').css({
-                'height' : menuHeight
-            });
-        }
-
-        /**
-         * monta o nome da classe css de acordo com a posição dele na tela
-         * @param element
-         * @param menuElement
-         * @returns {string}
-         */
-        function getAnimationClass(element, menuElement) {
-            var width = menuElement.parent().width();
-            var height = menuElement.parent().height();
-            var offsetBottom = $document.height() - ( height + angular.element(menuElement[0]).offset().top );
-            var offsetRight = $document.width() - ( width + angular.element(menuElement[0]).offset().left );
-            var x = width > offsetRight ? 'right' : 'left';
-            var y = height > offsetBottom ? 'bottom' : 'top';
-
-            return x + '-' + y;
-
-        }
-
-        /**
-         * define a posição do menu em relação ao botão responsavel pelo evento de click
-         * @param button
-         * @param menu
-         * @param animationOrientation
-         */
-        function definePosition(button, menu, animationOrientation) {
-            var menuWidth = menu.width();
-            var buttonWidth = button.width();
-            var menuHeight = menu.height();
-            var buttonHeight = button.height();
-
-            if( animationOrientation == 'left-top' ){
-                menu.css({
-                    'left'  : '0px',
-                    'top'   : '15px'
-                })
-            }
-            else if( animationOrientation == 'right-top' ){
-                menu.css({
-                    'left'  : (menuHeight + buttonHeight) * -1,
-                    'top'   : '15px'
-                })
-            }else if( animationOrientation == 'right-bottom' ){
-                menu.css({
-                    'left'  : (menuHeight + buttonHeight) * -1,
-                    'top'   : (menuWidth + buttonWidth) * -1
-                })
-            }else if( animationOrientation == 'left-bottom'){
-                menu.css({
-                    'left'  : '0px',
-                    'top'   : (menuWidth + buttonWidth) * -1
-                })
-            }
-        }
 
     }
 	function Template( element, attributes ) {
 	    return $CONFIG.path+'/controls/dropdown/dropdown-template.html';
 	}
 
-        function Controller() {
-        }
+    function Controller() {
+    }
 
     };
 
